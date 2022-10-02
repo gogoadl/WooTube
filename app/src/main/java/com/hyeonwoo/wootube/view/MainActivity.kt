12486a7fc64793eb4eebffb25012e459b9d497bb
HyeonWoo Park <gogoadl@naver.com>
2022-10-02 13:12:23 +0900

    유튜브 플레이어 뷰 테스트 성공

--- app/src/main/java/com/hyeonwoo/wootube/view/MainActivity.kt
+++ app/src/main/java/com/hyeonwoo/wootube/view/MainActivity.kt
@@ -3,10 +3,15 @@ package com.hyeonwoo.wootube.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 import com.hyeonwoo.wootube.R
 import com.hyeonwoo.wootube.network.WooTubeService
 import com.hyeonwoo.wootube.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kr.co.prnd.YouTubePlayerView
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,6 +23,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel.videoList()
 
+        val youTubePlayerView = findViewById<YouTubePlayerView>(R.id.youtubePlayer)
+        youTubePlayerView.play("yB1lWmEE0w0", object : YouTubePlayerView.OnInitializedListener {
+            override fun onInitializationFailure(
+                provider: YouTubePlayer.Provider,
+                result: YouTubeInitializationResult
+            ) {
 
+            }
+
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider,
+                player: YouTubePlayer,
+                wasRestored: Boolean
+            ) {
+
+            }
+
+        })
     }
 }
