7fd809acb980d2e921ca059526d3fdc7639443ae
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2022-10-04 23:11:40 +0900

    Update MainRecyclerViewAdapter.kt

--- app/src/main/java/com/hyeonwoo/wootube/view/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/hyeonwoo/wootube/view/MainRecyclerViewAdapter.kt
@@ -1,15 +1,16 @@
 package com.hyeonwoo.wootube.view
 
-import android.R
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.hyeonwoo.wootube.R
 import com.hyeonwoo.wootube.model.Item
 import kr.co.prnd.YouTubePlayerView
 
 
-class MainRecyclerViewAdapter(val videoList: List<Item>) : RecyclerView.Adapter<>() {
+class MainRecyclerViewAdapter(val videoList: List<Item>) : RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val youTubePlayerView: YouTubePlayerView
@@ -21,14 +22,16 @@ class MainRecyclerViewAdapter(val videoList: List<Item>) : RecyclerView.Adapter<
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_main_recyclerview, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.youTubePlayerView.play(videoList.get(position).id, null)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return videoList.size
     }
 }
