ce5c69e881d22a656aaf2236429676215ab3cb28
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2022-10-07 23:12:10 +0900

    에러 추적중

--- app/src/main/java/com/hyeonwoo/wootube/view/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/hyeonwoo/wootube/view/MainRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.hyeonwoo.wootube.view
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,25 +11,28 @@ import com.hyeonwoo.wootube.model.Item
 import kr.co.prnd.YouTubePlayerView
 
 
-class MainRecyclerViewAdapter(val videoList: List<Item>) : RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder>() {
+class MainRecyclerViewAdapter(val videoList: List<Item>) : RecyclerView.Adapter<MainRecyclerViewAdapter.MainViewHolder>() {
 
-    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    inner class MainViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val youTubePlayerView: YouTubePlayerView
         init {
             // Define click listener for the ViewHolder's View.
             youTubePlayerView = itemView.findViewById(R.id.video_view)
-
+            Log.d("findViewById", "findViewById video_view")
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_main_recyclerview, parent, false)
-        return ViewHolder(view)
+        Log.d("onCreateViewHolder", "onCreateViewHolder")
+        return MainViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        Log.d("onBindViewHolder", "onBindViewHolder 1")
         holder.youTubePlayerView.play(videoList.get(position).id, null)
+        Log.d("onBindViewHolder", "onBindViewHolder 2")
     }
 
     override fun getItemCount(): Int {
